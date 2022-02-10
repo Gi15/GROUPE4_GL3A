eff0600a493118129e5724c892f1f37b58e28662
Messomo-christy <99398418+Messomo-christy@users.noreply.github.com>
2022-02-10 11:56:05 +0100

    Add files via upload
    Creation de la classe Personne : MESSOMO

---
+++ Personne.java
@@ -0,0 +1,51 @@
+public class Personne {
+    String nom;
+    Montre montre;
+
+    public Personne() {
+    }
+
+    public Personne(String nom, Montre montre) {
+        this.nom = nom;
+        this.montre = montre;
+    }
+
+    //Methode pour faire porter une montre
+    public  void fairePorterMontre(Montre montre){
+        if (this.montre == null){
+            this.montre = montre;
+        }else {
+            System.out.println("elle a deja une montre");
+        }
+    }
+
+    //Methode Pour enlever une montre
+    public void enleverMontre(){
+        if (this.montre != null)
+            this.montre = null ;
+    }
+
+    // Fonction qui donne l'heure
+    public String donnerHeure(){
+        if (montre != null)
+            return montre.heure + " h " + montre.minute + " m";
+        else
+            return null;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public Montre getMontre() {
+        return montre;
+    }
+
+    public void setMontre(Montre montre) {
+        this.montre = montre;
+    }
+}
